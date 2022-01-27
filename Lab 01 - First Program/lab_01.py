58a245b536694e81a3ff4a10ed96d3575f4edca5
Chris Foree <chrisforee@gmail.com>
2022-01-27 16:52:28 -0600

    First Lab: Print and Escape Statements

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,3 +1,6 @@
 """First program in Learn Arcade Academy"""
+# Single line print statements
+print("Hello World")
+# Multiple line print statements
 print("Hello World")
 print("Hello again great big world")
