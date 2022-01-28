22b48dc268773ed14ced603e0d2bd377b3d0bb59
Chris Foree <chrisforee@gmail.com>
2022-01-28 15:03:42 -0600

    Testing Print Statements and Escape Codes;

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,6 +1,19 @@
-"""First program in Learn Arcade Academy"""
+"""First program in Learn Arcade Academy - Lab 01"""
 # Single line print statements
 print("Hello World")
 # Multiple line print statements
 print("Hello World")
-print("Hello again great big world")
+print("Hello World, says no one ever.")
+print("Someone really needs to change the name of everyone's first program. Hello World sucks, it just sucks!")
+# Escape codes and descriptions
+# \' Single quote
+# \" Double quote
+# \t Tab
+# \r Carriage return
+# \n Linefeed, moves cursor down
+# Escape Code for double quotations and printing the quote mark
+print("Hi!, This is a double quote mark: \"We use it for printing.")
+# Another example of an Escape code for quote marks
+print("My friend once told me, \"Dude, you should do programming or something.\".")
+# Example of a tab escape code
+print("Sometimes you just have to make a tab ladder:\tTab#1 \tTab#2 \tTab#3 \tTab#4")
