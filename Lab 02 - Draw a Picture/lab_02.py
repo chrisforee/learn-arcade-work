c292e4f4b070a7aa7e02873c2b1fc19c25284780
Chris Foree <chrisforee@gmail.com>
2022-01-29 08:51:39 -0600

    First drawing using arcade module and APIs; Lab 02

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -4,9 +4,11 @@ import arcade
 # Window parameters and name
 arcade.open_window(650, 650, "Learn Arcade Lab 02")
 # Set the background color
-arcade.set_background_color(arcade.csscolor.LIME_GREEN)
+arcade.set_background_color((141, 245, 66))
 # Ready to draw
 arcade.start_render()
+# Drawing circle/coin
+arcade.draw_circle_outline(325, 350, 150, (252, 252, 252), 5, 0, -1)
 arcade.finish_render()
 
 # Keep the window open
