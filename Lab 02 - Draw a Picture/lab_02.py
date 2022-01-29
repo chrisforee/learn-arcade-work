596e87f246995186302a2afba43d144ea196ae75
Chris Foree <chrisforee@gmail.com>
2022-01-28 19:19:20 -0600

    First drawing using arcade module and apis; Lab 02

---
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -0,0 +1,16 @@
+"""First drawing made using arcade module - Lab 02"""
+# Import the arcade library
+import arcade
+# Window parameters and name
+arcade.open_window(650, 650, "Learn Arcade Lab 02")
+# Set the background color
+arcade.set_background_color(arcade.csscolor.LIME_GREEN)
+# Ready to draw
+arcade.start_render()
+arcade.finish_render()
+
+# Keep the window open
+arcade.run()
+
+
+
