8ef848362c0eccdbe08986534b4945c7adabc7aa
Chris Foree <chrisforee@gmail.com>
2022-01-29 20:09:12 -0600

    First drawing using arcade module and APIs;"Alien Watching"

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -2,17 +2,23 @@
 # Import the arcade library
 import arcade
 # Window parameters and name
-arcade.open_window(650, 650, "Learn Arcade Lab 02")
+arcade.open_window(650, 650, "Learn Arcade Lab 02 - Alien Watching")
 # Set the background color
-arcade.set_background_color((141, 245, 66))
+arcade.set_background_color(arcade.csscolor.DARK_SLATE_GRAY)
 # Ready to draw
 arcade.start_render()
-# Drawing circle/coin
-arcade.draw_circle_outline(325, 350, 150, (252, 252, 252), 5, 0, -1)
+
+# Drawing alien head
+# Start with large circle
+arcade.draw_circle_filled(325, 125, 150, (141, 245, 66), 0, -1)
+arcade.draw_circle_outline(325, 125, 150, (252, 252, 252), 5, 0, -1)
+# Draw the eyes and mouth
+arcade.draw_ellipse_filled(275, 200, 98, 55, (0, 0, 0), 20, 10)
+arcade.draw_ellipse_filled(375, 200, 98, 55, (0, 0, 0), 160, 10)
+arcade.draw_line(305, 110, 345, 110, (0, 0, 0), 5)
+
+# Finished drawing
 arcade.finish_render()
 
 # Keep the window open
 arcade.run()
-
-
-
